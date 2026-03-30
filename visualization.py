c605ee39db33ffc9d2570df8020e80bbd9c776b8
danieljosemoca <daniel.jose.moca@gmail.com>
2026-03-31 01:05:55 +0200

    changes to plot_tree_2d() to have color represent distance from root node

--- visualization.py
+++ visualization.py
@@ -3,6 +3,12 @@ from tree import Node
 
 
 def plot_tree_2d(nodes: dict[int, Node]):
+    root = next(node for node in nodes.values() if node._parent is None)
+
+    # needed to normalize distance from root
+    max_dist = max(node.distance(root) for node in nodes.values())
+    cmap = plt.colormaps["copper"]
+
     for node in nodes.values():
         if node._parent is None:
             continue  # skip root node
@@ -10,10 +16,18 @@ def plot_tree_2d(nodes: dict[int, Node]):
         x_vals = [node._x, node._parent._x]
         y_vals = [node._y, node._parent._y]
 
-        plt.plot(x_vals, y_vals)  # plot a 2D line from current node to its parent node
+        distance_from_root = node.distance(root)
+
+        # pick color in colormap using normalized distance from root
+        color = cmap(distance_from_root / max_dist)
+
+        # plot a 2D line from current node to its parent node
+        # color from normalized distance from root
+        plt.plot(x_vals, y_vals, color=color)
 
     plt.xlabel("X position")
     plt.ylabel("Y position")
     plt.title("Neuron Morphology 2D")
+    plt.axis("equal")  # keeps scale of axes equal (not so 2D otherwise)
 
     plt.show()
