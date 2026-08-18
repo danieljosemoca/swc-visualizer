65e36f55f3736102f60e6597eb3e0d9fcc085d8d
Daniel Moreno <55993164+danieljosemoca@users.noreply.github.com>
2026-08-18 03:00:40 +0100

    Rename main function to visualize_swc and replace input call with visualization argument

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ import glob
 from visualization import plot_2d, plot_3d
 from processing import swc_to_dataframe, dataframe_to_tree
 
-
-def main() -> None:
+def visualize_swc(visualization = "3d") -> None:
     """Main function to run the program."""
     # locate all swc files in the 'morphology' folder
     swc_files = glob.glob("morphology/*.swc")
     if not swc_files:
         print("No .swc files found in a 'morphology' folder.")
         return
-
-    # ask user which visualization(s) they want
-    print("Which visualization(s) would you like?")
-    print("1: 2D only")
-    print("2: 3D only")
-    print("3: Both 2D and 3D")
-    choice = input("Enter 1, 2, or 3: ").strip()
-    do_2d = choice == "1" or choice == "3"  # do_2d is True is 2D selected for
-    do_3d = choice == "2" or choice == "3"  # do_3d is True is 3D selected for
-
+    
+    # determine visualization
+    visualization = visualization.upper()
+    if visualization == "2D": 
+        do_2d = True 
+        do_3d = False 
+    elif visualization == "3D":
+        do_2d = False 
+        do_3d = True 
+    elif visualization == "BOTH":
+        do_2d = True 
+        do_3d = True 
+    else:
+        msg = "Expected '2d', '3d', or 'both' for visualization argument"
+        raise ValueError(msg)
+        
     # process each file
     for filepath in swc_files:
         print(f"\n--- Processing: {os.path.basename(filepath)} ---")
@@ -41,4 +46,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    visualize_swc()
